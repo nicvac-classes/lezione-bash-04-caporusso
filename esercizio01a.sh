f25fa7394a720f334723d21aa7ad3aea16531e02
giuseppecap08 <giuseppe.caporusso@marconibari.edu.it>
2026-03-31 07:08:43 +0000

    esercizio01

---
+++ esercizio01a.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# ============================================================
+# VERIFICA BASH - Traccia A - Esercizio 1
+# Navigazione e directory
+# ============================================================
+# OBIETTIVO:
+#   1. Visualizza il percorso assoluto della directory corrente
+#   2. Entra nella cartella ambiente/documenti/report/2023
+#   3. Verifica di essere nella posizione giusta
+#   4. Torna alla radice del repository con un solo comando usando ..
+#
+# Scrivi i comandi qui sotto:
+# ------------------------------------------------------------
+@giuseppecap08 ➜ /workspaces/lezione-bash-04-caporusso (main) $ pwd
+/workspaces/lezione-bash-04-caporusso
+@giuseppecap08 ➜ /workspaces/lezione-bash-04-caporusso (main) $ cd ambiente/documenti/report/2023
+@giuseppecap08 ➜ .../ambiente/documenti/report/2023 (main) $ pwd
+/workspaces/lezione-bash-04-caporusso/ambiente/documenti/report/2023
+@giuseppecap08 ➜ .../ambiente/documenti/report/2023 (main) $ cd ../../../../
+@giuseppecap08 ➜ /workspaces/lezione-bash-04-caporusso (main) $
+
+
+
